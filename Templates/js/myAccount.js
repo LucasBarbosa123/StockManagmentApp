7ba4d92a4752ad4a20e8135ab084f1ea0847a0f3
LucasBarbosa123 <lucasobrab@yahoo.com>
2024-07-15 00:05:27 +0100

    almost nothing actually, i still need to make the endpoints for updating names and pass

---
+++ Templates/js/myAccount.js
@@ -0,0 +1,17 @@
+function ChangeNames() {
+
+}
+
+function StartChangePass() {
+    let changePassBtt = document.getElementById('changePassBtt')
+    let passActionTrigger = document.getElementById('passActionTrigger')
+    let passConfirmationContainer = document.getElementById('passConfirmationContainer')
+
+    changePassBtt.classList.remove('d-none')
+    passConfirmationContainer.classList.remove('d-none')
+    passActionTrigger.classList.add('d-none')
+}
+
+function ChangePass() {
+
+}
